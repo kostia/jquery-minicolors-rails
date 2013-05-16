afcd13ceed1c8fb63b217d62094bec791e1d64cf
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2013-05-16 16:03:48 +0200

    Minicolors input refactored

--- app/inputs/minicolors_input.rb
+++ app/inputs/minicolors_input.rb
@@ -1,12 +1,12 @@
 if defined? SimpleForm
   class MinicolorsInput < SimpleForm::Inputs::StringInput
     def input_html_options
-      options = super
-      options[:data] ||= {}
-      options[:data][:minicolors] ||= {}
-      options
+      super.tap do |options|
+        options[:data] ||= {}
+        options[:data][:minicolors] ||= {}
+      end
     end
 
-    def input_type; :text; end
+    def input_type; :text end
   end
 end
