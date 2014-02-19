7456b487215d8b5eef090c3708d32a58b5224a95
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-02-19 08:52:54 +0100

    Optional stylesheet using data-URL for background

--- lib/jquery-minicolors-rails.rb
+++ lib/jquery-minicolors-rails.rb
@@ -1,4 +1,8 @@
 require 'jquery-minicolors-rails/engine'
 
 module JqueryMinicolorsRails
+  def self.background_image_data_url
+    @_background_image_data_url ||=
+        File.read(File.expand_path('../../vendor/assets/images/jquery.minicolors.txt', __FILE__))
+  end
 end
