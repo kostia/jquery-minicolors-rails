bb4621ddaf6f83a84c9aac6c80e6855ba93aa504
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-02-21 01:13:14 +0100

    Load and cache image data URLs

--- lib/jquery-minicolors-rails.rb
+++ lib/jquery-minicolors-rails.rb
@@ -5,12 +5,23 @@ module JqueryMinicolorsRails
     attr_writer :use_data_urls
 
     def use_data_urls?
-      !!@use_data_urls
+      defined?(@use_data_urls) ? !!@use_data_urls : true
     end
 
-    def image_data_url
-      @image_data_url ||=
-          File.read(File.expand_path('../../vendor/assets/images/jquery.minicolors.txt', __FILE__))
+    def image_data_url(name)
+      image_data_url_cache.fetch(name) do
+        image_data_url_cache[:name] = load_image_data_url(name)
+      end
+    end
+
+    private
+
+    def image_data_url_cache
+      @image_data_url_cache ||= {}
+    end
+
+    def load_image_data_url(name)
+      File.read(File.expand_path("../../vendor/assets/images/jquery.minicolors.#{name}", __FILE__))
     end
   end
 end
