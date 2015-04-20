63e1d5188d159126940cf48e75fa8048b6aad6be
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2015-04-20 23:32:46 +0200

    Updated to be able to run with the newest rspec

--- spec/inputs/minicolors_input_spec.rb
+++ spec/inputs/minicolors_input_spec.rb
@@ -1,16 +1,12 @@
 require 'spec_helper'
 
 describe 'minicolors input' do
-  subject do
-    helper.simple_form_for :foo, url: '' do |f|
+  it 'passes minicolors settings' do
+    markup = helper.simple_form_for :foo, url: '' do |f|
       f.input :minicolors, as: :minicolors, input_html: {data: {minicolors: {theme: :bootstrap}}}
     end
-  end
-
-  it { should have_selector('input[data-minicolors]') }
-
-  it 'passes minicolors settings' do
-    MultiJson.decode(Nokogiri::HTML(subject).css('#foo_minicolors').attr('data-minicolors').text).
-        should eq({'theme' => 'bootstrap'})
+    doc = Nokogiri::HTML(markup)
+    expect(MultiJson.decode(doc.css('#foo_minicolors').attr('data-minicolors').text)).
+      to eq('theme' => 'bootstrap')
   end
 end
