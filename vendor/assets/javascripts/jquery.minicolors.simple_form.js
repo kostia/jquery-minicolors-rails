68ae658d533b160b8d972f84e0b3b95279675dc2
Daniel Prejean <danielp@aasteel.com>
2017-03-02 09:20:04 -0600

    Provide turbolinks support when using simple_form

--- vendor/assets/javascripts/jquery.minicolors.simple_form.js
+++ vendor/assets/javascripts/jquery.minicolors.simple_form.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).on('ready turbolinks:load', function {
   $('[data-minicolors]').each(function(i, elem) {
     var input = $(this);
     input.minicolors(input.data('minicolors'));
