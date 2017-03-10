f29f02859c92bed81415e68827d9a831fa6c8428
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2017-03-10 18:19:11 +0100

    Remove outdated config options from dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -47,12 +47,6 @@ module Dummy
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
 
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
-
     config.secret_key_base = 'spam'
 
     config.active_support.test_order = :sorted
--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -8,21 +8,6 @@ Dummy::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
-
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
@@ -44,9 +29,6 @@ Dummy::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
 
   # Enable threaded mode
   # config.threadsafe!
